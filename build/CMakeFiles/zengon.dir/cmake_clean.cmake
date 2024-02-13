4fa4dae8ea28f1adf41d014703d831a20dc4ab25
trevor <trevorleecox@icloud.com>
2024-02-13 14:16:27 -0600

    made a make file

---
+++ build/CMakeFiles/zengon.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zengon.dir/main.cpp.o"
+  "CMakeFiles/zengon.dir/main.cpp.o.d"
+  "zengon"
+  "zengon.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zengon.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/zengon.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for zengon.
+# This may be replaced when dependencies are built.
